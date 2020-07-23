15cd63bcbf0249ff6f5b3092bf85b4168c81b1af
squaresmile <squaresmile@protonmail.com>
2020-07-22 21:12:47 -0400

    PMS 1.20.0.3125

--- PlexMovieNew.bundle/Contents/Code/__init__.py
+++ PlexMovieNew.bundle/Contents/Code/__init__.py
@@ -3,17 +3,7 @@
 #
 
 class PlexMovieNewAgent(Agent.Movies):
-  name = 'Plex Movie New'
+  name = 'Plex Movie'
   primary_provider = True
-  languages = [Locale.Language.English, Locale.Language.Swedish, Locale.Language.French, 
-               Locale.Language.Spanish, Locale.Language.Dutch, Locale.Language.German, 
-               Locale.Language.Italian, Locale.Language.Danish,Locale.Language.Arabic, 
-               Locale.Language.Catalan, Locale.Language.Chinese, Locale.Language.Czech,
-               Locale.Language.Estonian, Locale.Language.Finnish, Locale.Language.Greek,
-               Locale.Language.Hebrew, Locale.Language.Hindi, Locale.Language.Hungarian,
-               Locale.Language.Indonesian, Locale.Language.Japanese, Locale.Language.Korean,
-               Locale.Language.Latvian, Locale.Language.Norwegian, Locale.Language.Persian,
-               Locale.Language.Polish, Locale.Language.Portuguese, Locale.Language.Romanian,
-               Locale.Language.Russian, Locale.Language.Slovak, Locale.Language.Thai,
-               Locale.Language.Turkish, Locale.Language.Ukrainian, Locale.Language.Vietnamese]
+  languages = []
 
